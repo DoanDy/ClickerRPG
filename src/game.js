660b7be0b9b0a948668b5b74105669e0b3d37b22
DoanDy <Gottaluvit@Kevins-MacBook-Air-2.local>
2017-05-08 23:37:48 +0700

    Renamed the images in the forest pack folder. Added a library with the phaser build files. Created a manifest and a chrome app host to run the game in Google Chrome.

--- src/game.js
+++ src/game.js
@@ -2,9 +2,24 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
 
 game.state.add('play', {
     preload: function() {
+        this.game.load.image('forest-back', 'assets/parallax_forest_pack/layers/p-FB-trees.png');
+        this.game.load.image('forest-front', 'assets/parallax_forest_pack/layers/p-FF-trees.png');
+        this.game.load.image('forest-lights', 'assets/parallax_forest_pack/layers/p-FL.png');
+        this.game.load.image('forest-middle', 'assets/parallax_forest_pack/layers/p-FM-trees.png');
+
         game.load.image('skeleton', 'assets/allacrost_enemy_sprites/skeleton.png');
     },
     create: function() {
+        var state = this;
+        this.background = this.game.add.group();
+        //setup each background layers.
+        ['forest-back', 'forest-lights', 'forest-middle', 'forest-front']
+        .foreEach(function(image) {
+          var bg = state.game.add.tileSprite(0, 0, state.game.world.width, state.game.world.height,
+          image, '', state.background);
+          bg.tileScale.setTo(4,4);
+        })
+
         var skeletonSprite = game.add.sprite(450, 290, 'skeleton');
         skeletonSprite.anchor.setTo(0.5, 0.5);
     },
